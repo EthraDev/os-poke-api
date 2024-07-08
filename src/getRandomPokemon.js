1175547c389ce0ae1037c8a2cdfa92875095131f
Thea Blachon <thea.blachon73@gmail.com>
2024-07-08 15:16:54 +0200

    ADD Version 1.0.1

--- src/getRandomPokemon.js
+++ src/getRandomPokemon.js
@@ -1,25 +1,32 @@
-const getRandomPokemon = async () => {
-  const pokemonId = Math.floor(Math.random() * 898) + 1; // Génère un ID entre 1 et 898
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  const data = await response.json();
+function getRandomPokemon() {
+  return new Promise((resolve, reject) => {
+    const pokemonId = Math.floor(Math.random() * 898) + 1; // Génère un ID entre 1 et 898
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const hp = data.stats.find(stat => stat.stat.name === 'hp').base_stat;
+        const attack = data.stats.find(stat => stat.stat.name === 'attack').base_stat;
+        const defense = data.stats.find(stat => stat.stat.name === 'defense').base_stat;
+        const speed = data.stats.find(stat => stat.stat.name === 'speed').base_stat;
 
-  // Extraire les statistiques pertinentes (HP, attack, defense, speed, etc.)
-  const hp = data.stats.find(stat => stat.stat.name === 'hp').base_stat;
-  const attack = data.stats.find(stat => stat.stat.name === 'attack').base_stat;
-  const defense = data.stats.find(stat => stat.stat.name === 'defense').base_stat;
-  const speed = data.stats.find(stat => stat.stat.name === 'speed').base_stat;
+        resolve({
+          name: data.name,
+          hp,
+          attack,
+          defense,
+          speed,
+          abilities: data.abilities.map(ability => ability.ability.name)
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
 
-  return {
-    name: data.name,
-    hp,
-    attack,
-    defense,
-    speed,
-    abilities: data.abilities.map(ability => ability.ability.name)
-  };
-};
-  
 export default getRandomPokemon;
