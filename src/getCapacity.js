f06d474e7ebae2c30513983460bd21c99e5f8e7d
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2024-07-09 09:38:00 +0200

    ADD JsDoc to getCapacity.js

--- src/getCapacity.js
+++ src/getCapacity.js
@@ -1,3 +1,25 @@
+/**
+ * Fetches abilities of Pokémon pick by it's ID from the PokeAPI.
+ * 
+ * @returns {Promise<Object>} A promise that resolves to a table containing all abilities form a Pokémon like this:
+ *  
+ *  [
+ *       {
+ *           ability: {
+ *               name: 'blaze',
+ *               url: 'https://pokeapi.co/api/v2/ability/66/'
+ *           },
+ *           is_hidden: false,
+ *           slot: 1
+ *       }
+ *  ]
+ * 
+ *  - {string[]} abilities - An array of abilities that the Pokémon has.
+ *  
+ * @throws Will throw an error if the network request fails or if the response is not ok.
+ */
+
+
 export default async function getPokemonCapacity(pokemonID)  {
 
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`
